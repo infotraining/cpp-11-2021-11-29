78293509e99796ff0854732d352eb0306b3b6a5b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-29 09:25:31 +0100

    start

---
+++ auto-decltype/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: D:/infotraining/code/cpp-11/code/_training_scaffolding/auto-decltype
+# Build directory: D:/infotraining/code/cpp-11/code/_training_scaffolding/auto-decltype/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "auto-decltype")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "D:/infotraining/code/cpp-11/code/_training_scaffolding/auto-decltype/CMakeLists.txt;50;add_test;D:/infotraining/code/cpp-11/code/_training_scaffolding/auto-decltype/CMakeLists.txt;0;")
