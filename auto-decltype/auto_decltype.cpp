cbdbc2c82e303ca85535d070817a392530182865
Krystian Piękoś <krystian.piekos@infotraining.pl>
2021-11-29 20:30:11 +0100

    Some clean-up

--- auto-decltype/auto_decltype.cpp
+++ auto-decltype/auto_decltype.cpp
@@ -1,16 +1,16 @@
 
 #include "catch.hpp"
+#include <array>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <list>
 #include <map>
-#include <array>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace Catch::Matchers;
 
-int foo(int arg) 
+int foo(int arg)
 {
     return arg;
 }
@@ -27,73 +27,71 @@ void deduce_2(T& arg)
     puts(__PRETTY_FUNCTION__);
 }
 
-// C++20
-// void deduce_1(auto arg)
-// {
-//     puts(__PRETTY_FUNCTION__);
-// }
-
 TEST_CASE("auto is almost the same as type arg deduction for function template")
 {
-    auto ax1 = 42;
-    auto ax2(42);
-    auto ax3 = ax1;
+    auto a1 = 42;
+    auto a2(42);
+    auto a3 = a1;
 
     deduce_1(42);
-    deduce_1(ax1);
+    deduce_1(a1);
 }
 
 TEST_CASE("auto type deduction - case 1")
 {
     std::cout << "\n--------------------\n";
     int x = 10;
-    auto ax1 = x; // int
+    auto a1 = x; // int
     deduce_1(x);
 
     const int cx = 42;
-    auto ax2 = cx; // int
+    auto a2 = cx; // int
     deduce_1(cx);
 
     int& ref_x = x;
-    auto ax3 = ref_x; // int
+    auto a3 = ref_x; // int
     deduce_1(ref_x);
 
     const int& cref_x = x;
-    auto ax4 = cref_x; // int
+    auto a4 = cref_x; // int
     deduce_1(cref_x);
 
     int tab[10];
-    auto ax5 = tab; // int* - decay to pointer
+    auto a5 = tab; // int* - decay to pointer
     deduce_1(tab);
 
-    auto ax6 = foo; // int(*ax6)(int) - decay to pointer
+    auto a6 = foo; // int(*ax6)(int) - decay to pointer
     deduce_1(foo);
 }
 
 TEST_CASE("auto type deduction - case 2")
 {
+    std::cout << "\n--------------------\n";
     int x = 10;
     auto& a1 = x; // int&
     deduce_2(x);
 
     const int cx = 42;
-    auto& a2 = cx; // const int& 
+    auto& a2 = cx; // const int&
+    deduce_2(cx);
 
     int& ref_x = x;
     auto& a3 = ref_x; // int&
+    deduce_2(ref_x);
 
     const int& cref_x = x;
     auto& a4 = cref_x; // const int&
+    deduce_2(cref_x);
 
     int tab[10];
     auto& a5 = tab; // int(&)[10] - no decay to pointer -> reference to array
     deduce_2(tab);
-        
+
     auto& a6 = foo; // int(&)(int) - no decay to pointer -> reference to function
     deduce_2(foo);
 }
 
-TEST_CASE("auto can be mixed with  modifiers")
+TEST_CASE("auto can be mixed with modifiers")
 {
     const auto a1 = 42;
 
@@ -104,19 +102,20 @@ TEST_CASE("auto can be mixed with  modifiers")
 
 TEST_CASE("using auto for iteration")
 {
+    std::cout << "\n--------------------\n";
     int data[10] = {1, 2, 3, 4};
     auto& view = data;
 
-    for(auto it = std::begin(data); it != std::end(data); ++it)
+    for (auto it = std::begin(data); it != std::end(data); ++it)
     {
         *it *= 2;
     }
 
-    for(auto it = std::cbegin(data); it != std::cend(data); ++it)
+    for (auto it = std::cbegin(data); it != std::cend(data); ++it)
     {
         std::cout << *it << " ";
-        std::cout << "\n";
     }
+    std::cout << "\n";
 }
 
 TEST_CASE("declaring variables with auto")
@@ -126,7 +125,7 @@ TEST_CASE("declaring variables with auto")
         int x1 = 10;
         int x2 = int(10);
         int x3(10);
-        int x4{10};
+        int x4 {10};
     }
 
     SECTION("with auto")
@@ -134,8 +133,8 @@ TEST_CASE("declaring variables with auto")
         auto ax1 = 10;
         auto ax2 = int(10);
         auto ax3(10);
-        auto ax4{10}; // !!! int - since C++17
-        //auto ax5{10, 11, 12}; // !!! ERROR - since C++17
+        auto ax4 {10}; // !!! int - since C++17
+        // auto ax5{10, 11, 12}; // !!! ERROR - since C++17
         auto ax6 = {1, 2, 3, 4}; // std::initializer_list<int>
     }
 }
@@ -143,9 +142,12 @@ TEST_CASE("declaring variables with auto")
 struct Argument
 {
     int value;
+
 private:
-    Argument(int v) : value{v}
-    {}
+    Argument(int v)
+        : value {v}
+    {
+    }
 };
 
 int foo(Argument arg)
@@ -155,12 +157,12 @@ int foo(Argument arg)
 
 TEST_CASE("decltype")
 {
-    std::map<int, std::string> dict = { { 1, "one"}, {2, "two"} };
+    std::map<int, std::string> dict = {{1, "one"}, {2, "two"}};
 
     auto backup = dict;
     REQUIRE(backup.size() == 2);
 
-    decltype(dict) other; 
+    decltype(dict) other;
     REQUIRE(other.size() == 0);
 
     using EntryType = decltype(dict)::value_type;
@@ -182,18 +184,18 @@ auto my_multiply(T1 a, T2 b)
     return a * b;
 }
 
-template<size_t N>
+template <size_t N>
 auto create_buffer()
 {
-    if constexpr(N <= 100)
-        return std::array<int, N>{};
-    else    
+    if constexpr (N <= 100)
+        return std::array<int, N> {};
+    else
         return std::vector<int>(N);
 }
 
 TEST_CASE("using auto in return type for function")
 {
-    auto result1 = my_multiply(1, 3.14);    
+    auto result1 = my_multiply(1, 3.14);
 
     auto small_buffer = create_buffer<64>();
     auto large_buffer = create_buffer<1024>();
@@ -201,7 +203,8 @@ TEST_CASE("using auto in return type for function")
 
 class Dictionary
 {
-    std::map<int, std::string> items = { {1, "one"}, {2, "two"} };
+    std::map<int, std::string> items = {{1, "one"}, {2, "two"}};
+
 public:
     auto get_by_value(int key) // int get_by_value(int key)
     {
@@ -213,7 +216,6 @@ public:
         return items[key];
     }
 };
-
 
 TEST_CASE("decltype(auto) vs. auto")
 {
